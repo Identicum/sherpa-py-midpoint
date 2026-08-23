0cb1bde2cfee8c02718551642d05b6fc8ebbe905
Gustavo J Gallardo <ggallard@identicum.com>
2026-08-23 11:25:19 -0300

    adjust Sherpa OIDs numbering scheme

--- sherpa/midpoint/midpoint_lib.py
+++ sherpa/midpoint/midpoint_lib.py
@@ -46,22 +46,23 @@ endpoints = {
 
 # For detail in OID numbering see: https://github.com/Identicum/sherpa-iga/blob/main/objects/OID.md
 # import_order: Numeric folder prefix (several classes can share a number)
-IDENTICUM_OID_BLOCK_B = "1de4"
+# Block A = IDENTICUM_OID_MARKER + 4-hex customer id ("0000" for base Sherpa objects).
+IDENTICUM_OID_MARKER = "1de4"
 object_types = [
-    {"class": "SystemConfigurationType",           "endpoint": "systemConfigurations",           "import_order": 3, "oid_block_d": None},
-    {"class": "UserType",                          "endpoint": "users",                          "import_order": 7, "oid_block_d": "0001"},
-    {"class": "ResourceType",                      "endpoint": "resources",                      "import_order": 4, "oid_block_d": "0002"},
-    {"class": "RoleType",                          "endpoint": "roles",                          "import_order": 5, "oid_block_d": "0004"},
-    {"class": "ObjectTemplateType",                "endpoint": "objectTemplates",                "import_order": 1, "oid_block_d": "0005"},
-    {"class": "TaskType",                          "endpoint": "tasks",                          "import_order": 8, "oid_block_d": "0007"},
-    {"class": "FunctionLibraryType",               "endpoint": "functionLibraries",              "import_order": 2, "oid_block_d": "0010"},
-    {"class": "ArchetypeType",                     "endpoint": "archetypes",                     "import_order": 2, "oid_block_d": "0011"},
-    {"class": "ValuePolicyType",                   "endpoint": "valuePolicies",                  "import_order": 2, "oid_block_d": "0012"},
-    {"class": "SecurityPolicyType",                "endpoint": "securityPolicies",               "import_order": 2, "oid_block_d": "0012"},
-    {"class": "ObjectCollectionType",              "endpoint": "objectCollections",              "import_order": 5, "oid_block_d": "0013"},
-    {"class": "AccessCertificationDefinitionType", "endpoint": "accessCertificationDefinitions", "import_order": 9, "oid_block_d": "0014"},
-    {"class": "SchemaType",                        "endpoint": "schemas",                        "import_order": 0, "oid_block_d": "0017"},
-    {"class": "ReportType",                        "endpoint": "reports",                        "import_order": 6, "oid_block_d": "0018"},
+    {"class": "SystemConfigurationType",           "endpoint": "systemConfigurations",           "import_order": 3, "oid_block_b": None},
+    {"class": "UserType",                          "endpoint": "users",                          "import_order": 7, "oid_block_b": "0001"},
+    {"class": "ResourceType",                      "endpoint": "resources",                      "import_order": 4, "oid_block_b": "0002"},
+    {"class": "RoleType",                          "endpoint": "roles",                          "import_order": 5, "oid_block_b": "0004"},
+    {"class": "ObjectTemplateType",                "endpoint": "objectTemplates",                "import_order": 1, "oid_block_b": "0005"},
+    {"class": "TaskType",                          "endpoint": "tasks",                          "import_order": 8, "oid_block_b": "0007"},
+    {"class": "FunctionLibraryType",               "endpoint": "functionLibraries",              "import_order": 2, "oid_block_b": "0010"},
+    {"class": "ArchetypeType",                     "endpoint": "archetypes",                     "import_order": 2, "oid_block_b": "0011"},
+    {"class": "ValuePolicyType",                   "endpoint": "valuePolicies",                  "import_order": 2, "oid_block_b": "0012"},
+    {"class": "SecurityPolicyType",                "endpoint": "securityPolicies",               "import_order": 2, "oid_block_b": "0012"},
+    {"class": "ObjectCollectionType",              "endpoint": "objectCollections",              "import_order": 5, "oid_block_b": "0013"},
+    {"class": "AccessCertificationDefinitionType", "endpoint": "accessCertificationDefinitions", "import_order": 9, "oid_block_b": "0014"},
+    {"class": "SchemaType",                        "endpoint": "schemas",                        "import_order": 0, "oid_block_b": "0017"},
+    {"class": "ReportType",                        "endpoint": "reports",                        "import_order": 6, "oid_block_b": "0018"},
 ]
 
 
@@ -88,24 +89,19 @@ def check_sherpa_oid(oid: str, object_class: str, expected_customer_id: str = "0
     if len(blocks) != 5:
         logger.error("oid '{}' does not have the expected 5 blocks.".format(oid))
         raise ValueError("oid '{}' does not have the expected 5 blocks.".format(oid))
-    block_a, block_b, block_c, block_d, _block_e = blocks
-    if block_a != "00000000":
-        logger.error("oid '{}' block A must be '00000000'.".format(oid))
-        raise ValueError("oid '{}' block A must be '00000000'.".format(oid))
-    if block_b != IDENTICUM_OID_BLOCK_B:
-        logger.error("oid '{}' block B must be '{}'.".format(oid, IDENTICUM_OID_BLOCK_B))
-        raise ValueError("oid '{}' block B must be '{}'.".format(oid, IDENTICUM_OID_BLOCK_B))
-    if block_c.lower() != expected_customer_id.lower():
-        logger.error("oid '{}' block C is '{}', expected '{}'.".format(oid, block_c, expected_customer_id))
-        raise ValueError("oid '{}' block C is '{}', expected '{}'.".format(oid, block_c, expected_customer_id))
+    block_a, block_b, _block_c, _block_d, _block_e = blocks
+    expected_block_a = IDENTICUM_OID_MARKER + expected_customer_id
+    if block_a.lower() != expected_block_a.lower():
+        logger.error("oid '{}' block A is '{}', expected '{}'.".format(oid, block_a, expected_block_a))
+        raise ValueError("oid '{}' block A is '{}', expected '{}'.".format(oid, block_a, expected_block_a))
     entry = get_object_type_entry(object_class)
     if entry is None:
         logger.error("No object_types entry found for class '{}'.".format(object_class))
         raise ValueError("No object_types entry found for class '{}'.".format(object_class))
-    expected_block_d = entry["oid_block_d"]
-    if expected_block_d is not None and block_d.lower() != expected_block_d.lower():
-        logger.error("oid '{}' block D is '{}', expected '{}' for class '{}'.".format(oid, block_d, expected_block_d, object_class))
-        raise ValueError("oid '{}' block D is '{}', expected '{}' for class '{}'.".format(oid, block_d, expected_block_d, object_class))
+    expected_block_b = entry["oid_block_b"]
+    if expected_block_b is not None and block_b.lower() != expected_block_b.lower():
+        logger.error("oid '{}' block B is '{}', expected '{}' for class '{}'.".format(oid, block_b, expected_block_b, object_class))
+        raise ValueError("oid '{}' block B is '{}', expected '{}' for class '{}'.".format(oid, block_b, expected_block_b, object_class))
 
 
 class MidpointError(Exception):
